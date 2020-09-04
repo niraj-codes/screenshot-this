a89a79a05fcdd7257a6c42e6d2723dc442dd1dcd
niraj-codes <31800419+niraj-codes@users.noreply.github.com>
2020-09-04 02:01:47 -0600

    Update this.py

--- CODE/this.py
+++ CODE/this.py
@@ -1,58 +1,65 @@
-import unittest
-from Unit_Test.test_class import *
+from tkinter import *
+from tkinter import messagebox
+import sqlite3
+from classes.searchingSortingAlgorithm import SearchingSorting
 
 
-class TestThis(unittest.TestCase):
-    the_class = TestClass()
+class IssueBook:
+    def issue_view(self):
+        self.aidd = StringVar()
+        self.astudentt = StringVar()
+        self.f1 = Frame(height=550, width=500, bg='black')
+        self.f1.place(x=500, y=100)
+        l1 = Label(self.f1, text='Book ID : ', font='papyrus 15 bold', bg='black', fg='orange').place(x=50, y=100)
+        e1 = Entry(self.f1, width=25, bd=4, bg='orange', textvariable=self.aidd).place(x=180, y=100)
+        l2 = Label(self.f1, text='Student Id : ', font='papyrus 15 bold', bg='black', fg='orange').place(x=50, y=150)
+        e2 = Entry(self.f1, width=25, bd=4, bg='orange', textvariable=self.astudentt).place(x=180, y=150)
+        b1 = Button(self.f1, text='Back', font='Papyrus 10 bold', fg='orange', bg='black', activebackground='orange',
+                    activeforeground='black', width=10, bd=3, command=self.f1.destroy).place(x=50, y=250)
+        b1 = Button(self.f1, text='Issue', font='Papyrus 10 bold', fg='orange', bg='black', activebackground='orange',
+                    activeforeground='black', width=10, bd=3, command=self.issue_book).place(x=200, y=250)
 
-    def testa_add_book(self):
-        result = self.the_class.add_book('1093', 'The Last Fish', 'A.J. Styles', 'Comedy', 58, '2889')
-        self.assertTrue(result)
+    def issue_book(self):
+        bookid = self.aidd.get()
+        studentid = self.astudentt.get()
+        conn = sqlite3.connect('db/softwarica.db')
+        cursor = conn.cursor()
 
-    def testj_add_book_unintended_input1(self):
-        result = self.the_class.add_book('1093', 'The Last Fish', 'A.J. Styles', 'Comedy', -98, '2889')
-        expected_result = False
-        self.assertEqual(result, expected_result)
+        #######manual
+        cursor.execute("select ID from book_info")
+        id_db_list = cursor.fetchall()
+        id_list = []
 
-    def testc_search_book(self):
-        result = self.the_class.search_book('1093')
-        self.assertTrue(result)
+        position = 0
+        while position < len(id_db_list):
+            id_list.append(id_db_list[position][0])
+            position = position + 1
+        SearchingSorting.selection_sort(self, id_list)
+        if SearchingSorting.linear_search(self, bookid, id_list):
+            ##########################
+            cursor.execute("select ID,COPIES from book_info where ID=?", (bookid.capitalize(),))
+            an = cursor.fetchall()
+            if bookid and studentid != "":
+                if an != []:
+                    for i in an:
+                        if i[1] > 0:
+                            try:
+                                conn.execute("insert into book_issued \
+                                values (?,?,date('now'),date('now','+7 day'))",
+                                             (bookid.capitalize(), studentid.capitalize(),));
+                                conn.commit()
+                                conn.execute("update book_info set COPIES=COPIES-1 where ID=?", (bookid.capitalize(),))
+                                conn.commit()
+                                conn.close()
+                                messagebox.showinfo("Updated", "Book Issued sucessfully.")
+                            except:
+                                messagebox.showerror("Error", "Book is already issued by student.")
 
-    def testd_issue_book(self):
-        result = self.the_class.issue_book('1093', '199333')
-        self.assertTrue(result)
-
-    def testk_add_book_unintended_input2(self):
-        result = self.the_class.add_book('1093', 'The Last Fish', '', 'Comedy', 98, '2889')
-        expected_result = False
-        self.assertEqual(result, expected_result)
-
-    def testl_return_non_existent_book(self):
-        result = self.the_class.return_book('99', '199333')
-        expected_result = False
-        self.assertEqual(result, expected_result)
-
-    def testf_return_book(self):
-        result = self.the_class.return_book('1093', '199333')
-        self.assertTrue(result)
-
-    def testb_list_all_books(self):
-        result = self.the_class.list_all_books()
-        expected_result = 7
-        self.assertEqual(result, expected_result)
-
-    def teste_search_all_activity(self):
-        result = self.the_class.search_all_activity()
-        expected_result = 10
-        self.assertEqual(result, expected_result)
-
-    def testg_delete_book(self):
-        result = self.the_class.delete_book()
-        self.assertTrue(result)
-
-
-if __name__ == '__main__':
-    unittest.main()
-
-    
-    
+                        else:
+                            messagebox.showerror("Unavailable", "Book unavailable.\nThere are 0 copies of the book.")
+                else:
+                    messagebox.showerror("Error", "No such Book in Database.")
+            else:
+                messagebox.showerror("Error", "Fields cannot be blank.")
+        else:
+            messagebox.showerror("Unavailable", "Book unavailable.")
